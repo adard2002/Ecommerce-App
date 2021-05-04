5877a52ec24fe4cc79ec25f883e624f6b5705249
unknown <adard2002@gmail.com>
2021-05-04 15:08:11 -0500

    added UploadProfile form/action

--- WebApplication1/Models/Identity/ApplicationUser.cs
+++ WebApplication1/Models/Identity/ApplicationUser.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,5 +9,7 @@ namespace WebApplication1.Models.Identity
 {
     public class ApplicationUser : IdentityUser<int>
     {
+        [StringLength(250)]
+        public string ProfileImageUrl { get; set; }
     }
 }
