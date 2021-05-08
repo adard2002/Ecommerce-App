9c281410e348cf2cd467b6b54a1691cd527d1cc7
unknown <adard2002@gmail.com>
2021-05-07 19:11:08 -0500

    added sendgrid but will add the actual api key next time i work on this

--- WebApplication1/Services/IEmailService.cs
+++ WebApplication1/Services/IEmailService.cs
@@ -1,11 +1,35 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebApplication1.Services
 {
-    interface IEmailService
+    public interface IEmailService
     {
+        Task SendEmail(string email, string subject);
+    }
+
+    public class NullEmailService : IEmailService
+    {
+        public Task SendEmail(string email, string subject)
+        {
+            return Task.CompletedTask;
+        }
+    }
+
+    public partial class LoggerEmailService : IEmailService
+    {
+        private readonly ILogger<LoggerEmailService> logger;
+        public LoggerEmailService(ILogger<LoggerEmailService> logger)
+        {
+            this.logger = logger;
+        }
+        public Task SendEmail(string email, string subject)
+        {
+            logger.LogInformation("Sending to {0} with subject and stuff '{2}'", email, subject);
+            return Task.CompletedTask;
+        }
     }
 }
